aa6d51454725df1d76f354c01d552f27962f1139
JoachimWilk <JoachimWilk@example.com>
2017-05-10 00:04:05 +0200

    new chapter added

---
+++ 1.5.1_mow_mi_python/mow_mi_python.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#deklarujemy i inicjalizujemy zmienne
+aktRok = 2017
+pythonRok = 1989
+#obliczamy wiek Pythona
+wiekPythona = aktRok - pythonRok
+
+#pobieramy dane
+imie = raw_input('Jak się nazywasz? ')
+wiek = int(raw_input('Ile masz lat? '))
+
+print 'Witaj', imie
+print "Mów mi Python, mam", wiekPythona, "lat."
+
+#instrukcja warunkowa
+if wiek > wiekPythona:
+    print "Jesteś starszy ode mnie."
+else:
+    print "Jesteś młodszy ode mnie."
