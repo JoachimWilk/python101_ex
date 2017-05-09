2bfea3d2a672fd283b5a723c445d25016bd442d3
JoachimWilk <JoachimWilk@example.com>
2017-05-09 23:42:34 +0200

    commit after full chapter

--- 1.4_extra_lotek/totomodul.py
+++ 1.4_extra_lotek/totomodul.py
@@ -2,23 +2,60 @@
 # -*- coding: utf-8 -*-
 
 import random
+import os
+import json
 
 
 def ustawienia():
-    """Funkcja pobiera ilość losowanych liczb, maksymalną losowaną wartość
-    oraz ilość prób. Pozwala określić stopień trudności gry."""
-    while True:
-        try:   
-            ile = int(raw_input("Podaj ilość typowanych liczb: "))
-            maks = int(raw_input("Podaj maksymalną losowaną liczbę: "))
-            if ile > maks:
+    """Funkcja pobiera nick użytkownika, ilość losowanych liczb,    
+    maksymalną losowaną wartość oraz ilość prób. 
+    Pozwala określić stopień trudności gry."""
+
+    nick = raw_input("Podaj nick: ")
+    nazwapliku = nick + ".ini"
+    gracz = czytaj_ust(nazwapliku)
+    odp = None
+
+    if gracz:
+        print "Twoje ustawienia:"
+        print "Liczb:", gracz[1]
+        print "Z Maks:", gracz[2]
+        print "Losowań:", gracz[3]
+        odp = raw_input("Zmieniasz (t/n)? ")
+    
+    if not gracz or odp.lower() == "t":  
+        while True:
+            try:   
+                ile = int(raw_input("Podaj ilość typowanych liczb: "))
+                maks = int(raw_input("Podaj maksymalną losowaną liczbę: "))
+                if ile > maks:
+                    print "Błędne dane!"
+                    continue
+                ilelos = int(raw_input("Ile losowań: "))
+                break
+            except:
                 print "Błędne dane!"
                 continue
-            ilelos = int(raw_input("Ile losowań: "))
-            return (ile, maks, ilelos)
-        except:
-            print "Błędne dane!"
-            continue
+        gracz = zapisz_ust(nazwapliku, [nick, str(ile), str(maks), str(ilelos)])
+
+    return gracz[0:1] + map(int, gracz[1:4])
+
+
+def czytaj_ust(nazwapliku):
+    if os.path.isfile(nazwapliku):
+        plik = open(nazwapliku, "r")
+        linia = plik.readline()
+        if linia:
+            return linia.split(";")
+    return False
+
+
+def zapisz_ust(nazwapliku, gracz):
+    plik = open(nazwapliku, "w")
+    plik.write(";".join(gracz))
+    plik.close()
+    return gracz
+ 
 
 def losujliczby(ile, maks):
     """Funkcja losuje ile unikalnych liczb całkowitych od 1 do maks"""
@@ -47,4 +84,56 @@ def pobierztypy(ile, maks):
             typy.add(typ)
             i = i + 1
     return typy
+
+
+def wyniki(liczby, typy):
+    trafione = liczby & typy
+    if trafione:
+        print "\nIlość trafień: %s" % len(trafione)
+        trafione2 = ", ".join(map(str, trafione))
+        print "Trafione liczby: %s" % trafione2
+    else:
+        print "Brak trafień. Spróbuj jeszcze raz!"
+
+    print "\n" + "x" * 40 + "\n" #wydrukuj 40 znaków x
+    return len(trafione)
+
+
+def czytaj_json(nazwapliku):
+    """Funkcja odczytuje dane w formacie json z pliku"""
+    dane = []
+    if os.path.isfile(nazwapliku):
+        with open(nazwapliku, "r") as plik:
+            dane = json.load(plik)
+    return dane
+
+
+def zapisz_json(nazwapliku, dane):
+    """Funkcja zapisuje dane w formacie json do pliku"""
+    with open(nazwapliku, "w") as plik:
+        json.dump(dane, plik)
+
+
+def zapisz_str(nazwapliku, dane):
+    """Funkcja zapisuje dane w formacie txt do pliku"""
+    with open(nazwapliku, "w") as plik:
+        for slownik in dane:
+            linia = [k + ":" + str(w) for k, w in slownik.iteritems()]
+            linia = ";".join(linia)
+            #plik.write(linia+"\n") - zamiast tak, można:
+            print >>plik, linia
+
+
+#TODO: napisac funkcje
+def czytaj_str(nazwapliku):
+    """Funkcja odczytuje dane w formacie txt z pliku"""
+    with open(nazwapliku, "r") as plik:
+        linia = plik.readline()
+    return
+
+
+
+
+
+
 
